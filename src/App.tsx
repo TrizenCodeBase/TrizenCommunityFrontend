215072adddac8481eae6cbef7aefbbb1582fedc8
Yasha <yasha@example.com>
2025-10-22 15:05:25 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,12 @@ const App = () => (
       <AuthProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true
+          }}
+        >
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/events" element={<Events />} />
